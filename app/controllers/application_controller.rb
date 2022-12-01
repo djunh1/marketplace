5e143c0dfaa901c2dd14e272e24c0681c21c0f28
djunh1 <djunh1@gmail.com>
2022-12-01 17:43:36 -0500

    Add the administrate gem.  Change default url.  Only allow site admins to access.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-
+    before_action :set_variables
     include CurrentUserConcern
+
+    def set_variables
+        #Set these for application wide values.
+         @company_name = "The Desk (TM, LLC, PLLC, Esquire, CMT, Dr etc)"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin, path: 'market_place_admin' do 
+      resources :users
+      resources :guest_users
+
+      root to: "users#index"
+    end
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
   get '/main' => 'pages#landing_page'
